945df27d65c88f2d3cc5ee7a171b0b8d853e6bf1
Furkan Börek <furkanb_001@outlook.com>
2019-11-05 19:04:35 +0300

    MVCUI yapıldı ve proje tamamen kontrol edıldı kusursuz calısıyor.

---
+++ Project.MVCUI/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Project.Bll.DesignPatterns.SinglettonPattern;
+using Project.Dal.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.MVCUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyContext db;
+        public HomeController()
+        {
+            db = DBTool.DBInstance;
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+    }
+}
